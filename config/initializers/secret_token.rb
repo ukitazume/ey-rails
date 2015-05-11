29a3e70f408f7d5fe39d37c24a58bb1038a04a28
Yu Kitazume <u.kitazume@gmail.com>
2015-05-11 13:54:45 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EyRails::Application.config.secret_token = '03700f17257094eb1b6d781fb1db6bf3bf2b4c460edb3cd1388bdaf41ae75769ce6ab6e54da0c1185d43dee6930e867a624d4409b222a01a39e08f488fecada1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+EyRails::Application.config.session_store :cookie_store, key: '_ey-rails_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# EyRails::Application.config.session_store :active_record_store
