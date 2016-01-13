958960d3ff5c2e60c4923af444f8400f8356fd6b
Yu Kitazume <u.kitazume@gmail.com>
2016-01-13 13:40:56 +0900

    link node_module

---
+++ deploy/before_bundle.rb
@@ -0,0 +1,3 @@
+shared_node_modules = "#{config.shared_path}/node_modules"
+run "if [ -d #{shared_node_modules} ]; then mkdir #{shared_node_modules}; fi"
+run "ln -nfs #{shared_node_modules} #{config.release_path}/node_modules"
